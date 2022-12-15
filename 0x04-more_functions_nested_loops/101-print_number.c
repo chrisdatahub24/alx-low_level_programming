f1d2863c0063cb61da75fbd8d357477e3d25ca68
CHrisDataHub <chrisdatahub@gmail.com>
2022-12-15 23:17:28 +0100

    more functions nested loops

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - Print a given an integer number to the stdout
+ * @n: An integer to be displayed
+ *
+ * Return: Nothing
+ */
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		num = -num;
+		_putchar('-');
+	}
+
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	_putchar((num % 10) + '0');
+}
