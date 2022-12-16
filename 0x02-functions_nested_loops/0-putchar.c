1ed36bf4d1669a27b385e1e7d969e403d1bc0e97
CHrisDataHub <chrisdatahub@gmail.com>
2022-12-16 19:44:37 +0100

    putchar commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - print putchar
+ * main -Entry point of the program
  *
- * Return: 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char txt[8] = "_putchar";
-	int i;
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-	for (i = 0; i < 8; i++)
-	{
-		putchar(txt[i]);
-	}
-	putchar('\n');
 	return (0);
 }
