bad219c5bdfd0c1fb86ca27a81a5d3267f407d8a
CHrisDataHub <chrisdatahub@gmail.com>
2022-12-20 04:51:20 +0100

    pointers arrays and strings

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
+
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -12,5 +13,6 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 
 #endif /* MAIN_H */
