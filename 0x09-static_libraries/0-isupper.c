0145029ee67c3ffb8347052ea9a6bff6cc054fd4
CHrisDataHub <chrisdatahub@gmail.com>
2023-01-09 09:30:30 +0100

    static libraries

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,10 @@
+/**
+ * _isupper - check if character is upper case
+ * @c: character to be checked
+ *
+ * Return: 1 if uppercase 0 otherwhise
+ */
+int _isupper(int c)
+{
+	return ('A' <= c && c <= 'Z' ? 1 : 0);
+}
